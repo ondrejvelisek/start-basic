2f7bbce17ce0b970a4c3b1cb50dbbd0200170fa7
Ondřej Velíšek <ondrejvelisek@gmail.com>
2024-11-20 19:36:10 +0100

    show cookie duplication

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { setCookie } from "vinxi/http";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Home,
-})
+  loader() {
+    console.info("Loading Home...");
+    setCookie("cookie-name", "cookie-value-1");
+    setCookie("cookie-name", "cookie-value-2");
+    setCookie("cookie-name", "cookie-value-3");
+    setCookie("cookie-name", "cookie-value-4");
+    setCookie("cookie-name", "cookie-value-5");
+    setCookie("cookie-name", "cookie-value-6");
+  },
+});
 
 function Home() {
   return (
