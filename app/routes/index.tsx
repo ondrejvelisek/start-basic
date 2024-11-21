e02d9ccf114f01071e53ec59e1916232f4b93a92
Ondřej Velíšek <ondrejvelisek@gmail.com>
2024-11-21 08:48:48 +0100

    extend example with createServerFn

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/start";
 import { setCookie } from "vinxi/http";
+
+const serverCookie = createServerFn().handler(async () => {
+  console.info(`Adding cookie`);
+  setCookie("cookie-name", "cookie-value-1");
+  setCookie("cookie-name", "cookie-value-2");
+  setCookie("cookie-name", "cookie-value-3");
+  setCookie("cookie-name", "cookie-value-4");
+});
 
 export const Route = createFileRoute("/")({
   component: Home,
-  loader() {
+  async loader() {
     console.info("Loading Home...");
-    setCookie("cookie-name", "cookie-value-1");
-    setCookie("cookie-name", "cookie-value-2");
-    setCookie("cookie-name", "cookie-value-3");
-    setCookie("cookie-name", "cookie-value-4");
-    setCookie("cookie-name", "cookie-value-5");
-    setCookie("cookie-name", "cookie-value-6");
+    await serverCookie();
+
+    if (import.meta.env.SSR) {
+      setCookie("cookie-name-2", "cookie-value-1");
+      setCookie("cookie-name-2", "cookie-value-2");
+      setCookie("cookie-name-2", "cookie-value-3");
+      setCookie("cookie-name-2", "cookie-value-4");
+    }
   },
 });
 
@@ -19,5 +30,5 @@ function Home() {
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
     </div>
-  )
+  );
 }
